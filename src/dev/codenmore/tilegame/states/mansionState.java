1d6f5369746fdfda11f9a216ed6ab2f1d8cc796b
kirbyhugs12 <maggiezh741999@gmail.com>
2016-04-29 11:12:13 -0400

    Update mansionState.java

--- src/dev/codenmore/tilegame/states/mansionState.java
+++ src/dev/codenmore/tilegame/states/mansionState.java
@@ -1,5 +1,7 @@
 package dev.codenmore.tilegame.states;
 
+import java.awt.Color;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -22,7 +24,7 @@ public class mansionState extends State{
 	public void tick() {
 		// TODO Auto-generated method stub
 		//System.out.println("hi");
-		if(handler.getMouseManager().getMouseX() > 20 && handler.getMouseManager().getMouseX() < 80 && handler.getMouseManager().getMouseY() > 420 && handler.getMouseManager().getMouseY() < 470 && handler.getMouseManager().isLeftPressed()) {
+		if(handler.getMouseManager().getMouseX() > 10 && handler.getMouseManager().getMouseX() < 70 && handler.getMouseManager().getMouseY() > 10 && handler.getMouseManager().getMouseY() < 60 && handler.getMouseManager().isLeftPressed()) {
 			State.setState(handler.getGame().mapState);
 		}
 		if(handler.getMouseManager().getMouseX() > 430 && handler.getMouseManager().getMouseX() < 500 && handler.getMouseManager().getMouseY() > 200 && handler.getMouseManager().getMouseY() < 270 && handler.getMouseManager().isLeftPressed()) {
@@ -37,10 +39,20 @@ public class mansionState extends State{
 		g.drawImage(hall, 0,0, null);
 		mapIcon = ImageLoader.loadImage("/textures/mapIcon.png");
 		mapIcon = resize(mapIcon,60,50);
-		g.drawImage(mapIcon, 20, 420,null);
+		g.drawImage(mapIcon, 10, 10, null);
 		rightArrow = ImageLoader.loadImage("/textures/rightarrow.png");
 		rightArrow = resize(rightArrow,70,70);
+		
 		g.drawImage(rightArrow, 430, 200,null);
+		g.setColor(Color.white);
+		g.fillRect(0, handler.getHeight()-50, handler.getWidth(), 50);
+		g.setColor(Color.black);
+		g.drawRect(0, handler.getHeight()-50, handler.getWidth(), 50);
+		g.drawLine(handler.getWidth()/5, handler.getHeight()-50, handler.getWidth()/5, handler.getHeight());
+		g.drawLine(2*handler.getWidth()/5, handler.getHeight()-50, 2*handler.getWidth()/5, handler.getHeight());
+		g.drawLine(3*handler.getWidth()/5, handler.getHeight()-50, 3*handler.getWidth()/5, handler.getHeight());
+		g.drawLine(4*handler.getWidth()/5, handler.getHeight()-50, 4*handler.getWidth()/5, handler.getHeight());
+		
 	}
 	
 	public BufferedImage resize(BufferedImage img, int newW, int newH) { 
