0169de41e363b2357f3bb4fd318abbac72ccc652
kirbyhugs12 <maggiezh741999@gmail.com>
2016-05-20 00:12:02 -0400

    Update jazzPRoomState.java

--- src/dev/codenmore/tilegame/states/jazzPRoomState.java
+++ src/dev/codenmore/tilegame/states/jazzPRoomState.java
@@ -1,5 +1,8 @@
 package dev.codenmore.tilegame.states;
 
+import java.awt.Color;
+
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -8,9 +11,14 @@ import java.awt.image.BufferedImage;
 import dev.codenmore.tilegame.Handler;
 import dev.codenmore.tilegame.gfx.ImageLoader;
 
-public class mansionArcadeState extends State{
+public class jazzPRoomState extends State {
 	private BufferedImage mapIcon;
-	public mansionArcadeState(Handler handler) {
+	private BufferedImage rightArrow;
+	private BufferedImage leftArrow;
+	private BufferedImage upArrow;
+	private Font fnt0;
+	
+	public jazzPRoomState(Handler handler) {
 		super(handler);
 		// TODO Auto-generated constructor stub
 	}
@@ -25,18 +33,29 @@ public class mansionArcadeState extends State{
 
 	@Override
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
+		fnt0 = new Font("arial", Font.BOLD, 40);
+		g.setFont(fnt0);
+		g.setColor(Color.black);
+		g.drawString("Private Room", 30, 720);
 		mapIcon = ImageLoader.loadImage("/textures/mapIcon.png");
-		mapIcon = resize(mapIcon,60,50);
+		mapIcon = resize(mapIcon,90,75);
 		g.drawImage(mapIcon, 10, 10, null);
+		rightArrow = ImageLoader.loadImage("/textures/rightarrow.png");
+		g.drawImage(rightArrow, 850, 300,null);
+		leftArrow = ImageLoader.loadImage("/textures/leftarrow.png");
+		g.drawImage(leftArrow, 15, 300,null);
+		upArrow = ImageLoader.loadImage("/textures/uparrow.png");
+		g.drawImage(upArrow, 450, 5,null);
 	}
-
+	
 	public BufferedImage resize(BufferedImage img, int newW, int newH) { 
 	    Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
 	    BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
 	    Graphics2D g2d = dimg.createGraphics();
 	    g2d.drawImage(tmp, 0, 0, null);
 	    g2d.dispose();
+
 	    return dimg;
-	} 
+	}
+
 }
