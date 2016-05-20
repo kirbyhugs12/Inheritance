bef45da5042f4c2da46efb501fd388e441174648
kirbyhugs12 <maggiezh741999@gmail.com>
2016-05-20 00:14:37 -0400

    Update mansionState.java

--- src/dev/codenmore/tilegame/states/mansionState.java
+++ src/dev/codenmore/tilegame/states/mansionState.java
@@ -2,19 +2,23 @@ package dev.codenmore.tilegame.states;
 
 import java.awt.Color;
 
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 import dev.codenmore.tilegame.Handler;
 import dev.codenmore.tilegame.gfx.ImageLoader;
 
 public class mansionState extends State{
+	private Font fnt0;
 	private BufferedImage hall;
 	private BufferedImage mapIcon;
 	private BufferedImage rightArrow;
-
+	private BufferedImage leftArrow;
+	private BufferedImage upArrow;
 	public mansionState(Handler handler) {
 		super(handler);
 		// TODO Auto-generated constructor stub
@@ -27,31 +31,34 @@ public class mansionState extends State{
 		if(handler.getMouseManager().getMouseX() > 10 && handler.getMouseManager().getMouseX() < 70 && handler.getMouseManager().getMouseY() > 10 && handler.getMouseManager().getMouseY() < 60 && handler.getMouseManager().isLeftPressed()) {
 			State.setState(handler.getGame().mapState);
 		}
-		if(handler.getMouseManager().getMouseX() > 430 && handler.getMouseManager().getMouseX() < 500 && handler.getMouseManager().getMouseY() > 200 && handler.getMouseManager().getMouseY() < 270 && handler.getMouseManager().isLeftPressed()) {
+		if(handler.getMouseManager().getMouseX() > 850 && handler.getMouseManager().getMouseX() < 1000 && handler.getMouseManager().getMouseY() > 350 && handler.getMouseManager().getMouseY() < 450 && handler.getMouseManager().isLeftPressed()) {
 			State.setState(handler.getGame().mansionArcadeState);
+			
+		}
+		if(handler.getMouseManager().getMouseX() > 0 && handler.getMouseManager().getMouseX() < 150 && handler.getMouseManager().getMouseY() > 350 && handler.getMouseManager().getMouseY() < 450 && handler.getMouseManager().isLeftPressed()) {
+			State.setState(handler.getGame().mansionGardenState);
+			
 		}
 	}
 	@Override
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
-		hall = ImageLoader.loadImage("/textures/photo.JPG");
-		hall = resize(hall,handler.getWidth(), handler.getHeight());
+		
+		hall = ImageLoader.loadImage("/textures/mansion_hallway.png");
 		g.drawImage(hall, 0,0, null);
+		fnt0 = new Font("arial", Font.BOLD, 40);
+		g.setFont(fnt0);
+		g.setColor(Color.black);
+		g.drawString("Main Hall", 30, 720);
 		mapIcon = ImageLoader.loadImage("/textures/mapIcon.png");
-		mapIcon = resize(mapIcon,60,50);
+		mapIcon = resize(mapIcon,90,75);
 		g.drawImage(mapIcon, 10, 10, null);
 		rightArrow = ImageLoader.loadImage("/textures/rightarrow.png");
-		rightArrow = resize(rightArrow,70,70);
+		g.drawImage(rightArrow, 850, 300,null);
+		leftArrow = ImageLoader.loadImage("/textures/leftarrow.png");
+		g.drawImage(leftArrow, 15, 300,null);
+		upArrow = ImageLoader.loadImage("/textures/uparrow.png");
+		g.drawImage(upArrow, 450, 5,null);
 		
-		g.drawImage(rightArrow, 430, 200,null);
-		g.setColor(Color.white);
-		g.fillRect(0, handler.getHeight()-50, handler.getWidth(), 50);
-		g.setColor(Color.black);
-		g.drawRect(0, handler.getHeight()-50, handler.getWidth(), 50);
-		g.drawLine(handler.getWidth()/5, handler.getHeight()-50, handler.getWidth()/5, handler.getHeight());
-		g.drawLine(2*handler.getWidth()/5, handler.getHeight()-50, 2*handler.getWidth()/5, handler.getHeight());
-		g.drawLine(3*handler.getWidth()/5, handler.getHeight()-50, 3*handler.getWidth()/5, handler.getHeight());
-		g.drawLine(4*handler.getWidth()/5, handler.getHeight()-50, 4*handler.getWidth()/5, handler.getHeight());
 		
 	}
 	
