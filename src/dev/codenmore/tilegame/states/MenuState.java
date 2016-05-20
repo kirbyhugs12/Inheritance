3af34487a50c72be7283f2b01a3a1225c1412288
kirbyhugs12 <maggiezh741999@gmail.com>
2016-05-20 00:03:36 -0400

    Update MenuState.java

--- src/dev/codenmore/tilegame/states/MenuState.java
+++ src/dev/codenmore/tilegame/states/MenuState.java
@@ -1,6 +1,7 @@
 package dev.codenmore.tilegame.states;
 
 import java.awt.Color;
+
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -10,36 +11,39 @@ import dev.codenmore.tilegame.Game;
 import dev.codenmore.tilegame.Handler;
 
 public class MenuState extends State{
-
+	private Font fnt0;
+	private Font fnt1;
 	public MenuState(Handler handler){
 		super(handler);
 	}
 	
-	public Rectangle playButton = new Rectangle(200,200,100,50);
-	public Rectangle instructionsButton = new Rectangle(200,300,100,50);
+	public Rectangle playButton = new Rectangle(435,300,130,65);
+	public Rectangle instructionsButton = new Rectangle(435,450,130,65);
 	@Override
 	
 	public void tick() {
-		if(handler.getMouseManager().getMouseX() >= 200 && handler.getMouseManager().getMouseX() <= 300 && handler.getMouseManager().getMouseY() >=200 && handler.getMouseManager().getMouseY() <=250 && handler.getMouseManager().isLeftPressed()) {
+		if(handler.getMouseManager().getMouseX() >= 435 && handler.getMouseManager().getMouseX() <= 565 && handler.getMouseManager().getMouseY() >=300 && handler.getMouseManager().getMouseY() <=365 && handler.getMouseManager().isLeftPressed()) {
 			State.setState(handler.getGame().mapState);
 		}
 		
-		//System.out.println(handler.getMouseManager().getMouseX() + "   " + handler.getMouseManager().getMouseY());
+		System.out.println(handler.getMouseManager().getMouseX() + "   " + handler.getMouseManager().getMouseY());
 	}
 
 	@Override
 	public void render(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		Font fnt0 = new Font(Font.MONOSPACED, Font.PLAIN, 50);
+		g2d.draw(instructionsButton);
+		g2d.draw(playButton);
+		fnt0 = new Font("arial", Font.BOLD, 80);
+		fnt1 = new Font("arial",Font.BOLD, 45);
+		
 		g.setFont(fnt0);
 		g.setColor(Color.black);
-		g.drawString("Inheritance", 90, 100);
-		Font fnt1 = new Font("arial",Font.BOLD, 30);
+		g.drawString("Inheritance", 290, 200);
 		g.setFont(fnt1);
-		g.drawString("Play", playButton.x +20, playButton.y + 36);
-		g2d.draw(playButton);
-		g.drawString("Rules", instructionsButton.x +10, instructionsButton.y + 36);
-		g2d.draw(instructionsButton);
+		g.drawString("Play", playButton.x +20, playButton.y + 50);
+		g.drawString("Rules", instructionsButton.x +6, instructionsButton.y + 50);
+		
 		
 	}
 
